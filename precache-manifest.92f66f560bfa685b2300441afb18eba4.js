b02a4c47abd5c61f259cd5f26a391e3fe50cacad
luu.hoait <luu.hoait@gmail.com>
2020-09-21 11:58:03 +0700

    Updates

---
+++ precache-manifest.92f66f560bfa685b2300441afb18eba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70f8613b3c65ea58f57f97531b2e8a2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "13cfaa2c537824bd9cf1",
+    "url": "/monsters-rolodex/static/css/main.1c8797f6.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cfaa2c537824bd9cf1",
+    "url": "/monsters-rolodex/static/js/main.80a1ab45.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
